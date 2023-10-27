0c1a8cc764993218d730322629ab268fa17ca6f2
SkylineBoi <minhajmrahman@gmail.com>
2023-10-27 01:16:35 -0400

    first

---
+++ HAH/helpers/config.js
@@ -0,0 +1,20 @@
+//config.js
+
+module.exports ={ 
+TEST_URL : 'https://tax-review-board-test.phila.gov/auth/?env:test',
+AD_USER_NAME : 'minhaj@sedev.testinator.com',
+AD_USER_PASSWORD : 'IlikeAlicePizza',
+commentTest : 'This is just for test purpose, please ignore, thank you!',
+businessName : 'Business Name Test LLC',
+Biz_EIN : '657278101',
+caseDescription : 'This petition is created using test automation, please ignore, thank you!',
+AD_PHONE_NUMBER : '1234567890',
+AD_MAILING_ADDRESS : '1234 Market St',
+AD_ZIP_CODE : '19107',
+AD_CITY : 'PHILADELPHIA',
+AD_STATE : 'Pennsylvania',
+AD_ACCNUMBER : '0354622000226005',
+DIS_PERIOD_START : 'January 2021',
+DIS_PERIOD_END : 'September 2023',
+FILE_PATH : 'ui//TestAutomation//helpers//Testfile.pdf',
+};
